b2569a39d7b563b87d7e88645ed6c45f7672bbbf
BIVI <danik.yura1@mail.ru>
2019-09-09 19:51:28 +0300

    new images and modified index.php

--- index.php
+++ index.php
@@ -1,27 +1,49 @@
-<style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
         table {
+            margin: 0 auto;
             border: 1px solid black;
-            margin: 50px auto;
         }
-
-        img {
+        
+       img {
             width: 300px;
             height: 250px;
         }
 </style>
-<table>
-        <tr>
-            <?php
-                $getImages = scandir("images");
-                unset($getImages[0], $getImages[1]);
-                foreach ($getImages as $img) {?>
-                    <td>
-                        <img src=<?="images/".$img?> alt="photo">
-                    </td>
-            <?php }?>
-        </tr>
-</table>
-
-
-    
-
+</head>
+<body> 
+    <?php
+        $getImages = scandir("images");
+        unset($getImages[0], $getImages[1]);
+        $i = 0;
+        function newString() { ?>
+            <tr></tr>
+            <?php };
+    ?>
+    <table>
+        
+            <?php      
+                foreach ($getImages as $img) {
+                    $i = ($i+1)%5;
+                    if ($i == 0) { ?>
+                        <td>
+                            <img src=<?php echo"images/".$img?> alt="photo">
+                        </td>
+                       
+                       <?php newString();
+                        
+                        
+                    } else {?>
+                        <td>
+                            <img src=<?php echo"images/".$img?> alt="photo">
+                        </td>
+                    <?php  }}?>
+    </table> 
+</body>
+</html>
